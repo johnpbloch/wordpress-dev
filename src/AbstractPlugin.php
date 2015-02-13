b14d6716c33bde885f5d6c0bd9ba5ae2c85182f3
John P Bloch <johnpbloch@gmail.com>
2015-02-13 00:01:57 -0600

    Add an interface and abstract base class for plugin objects

---
+++ src/AbstractPlugin.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace JPB\WP\Dev;
+
+abstract class AbstractPlugin implements PluginInterface {
+
+	use Hooks;
+
+	/** @var string */
+	protected $directory;
+
+	/** @var string */
+	protected $url;
+
+	/**
+	 * Run the plugin's main initialization routine
+	 *
+	 * @return PluginInterface Returns itself for easier method chaining
+	 */
+	public function run() {
+		$this->addAction( 'plugins_loaded', 'pluginsLoaded' );
+
+		return $this;
+	}
+
+	public function disable() {
+		$this->removeAction( 'plugins_loaded', 'pluginsLoaded' );
+	}
+
+	/**
+	 * Get the plugin directory path
+	 *
+	 * @return string
+	 */
+	public function getDirectory() {
+		return $this->directory;
+	}
+
+	/**
+	 * Set the plugin's directory path
+	 *
+	 * @param string $directory
+	 *
+	 * @return PluginInterface Returns itself for easier method chaining
+	 */
+	public function setDirectory( $directory ) {
+		$this->directory = $directory;
+
+		return $this;
+	}
+
+	/**
+	 * Get the plugin's url
+	 *
+	 * @return string
+	 */
+	public function getUrl() {
+		return $this->url;
+	}
+
+	/**
+	 * Set the plugin's url
+	 *
+	 * @param string $url
+	 *
+	 * @return PluginInterface Returns itself for easier method chaining
+	 */
+	public function setUrl( $url ) {
+		$this->url = $url;
+
+		return $this;
+	}
+
+	protected abstract function pluginsLoaded();
+
+}
---
+++ src/PluginInterface.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace JPB\WP\Dev;
+
+interface PluginInterface {
+
+	/**
+	 * Run the plugin's main initialization routine
+	 *
+	 * @return PluginInterface Returns itself for easier method chaining
+	 */
+	public function run();
+
+	/**
+	 * Get the plugin directory path
+	 *
+	 * @return string
+	 */
+	public function getDirectory();
+
+	/**
+	 * Set the plugin's directory path
+	 *
+	 * @param string $directory
+	 *
+	 * @return PluginInterface Returns itself for easier method chaining
+	 */
+	public function setDirectory( $directory );
+
+	/**
+	 * Get the plugin's url
+	 *
+	 * @return string
+	 */
+	public function getUrl();
+
+	/**
+	 * Set the plugin's url
+	 *
+	 * @param string $url
+	 *
+	 * @return PluginInterface Returns itself for easier method chaining
+	 */
+	public function setUrl( $url );
+
+}
