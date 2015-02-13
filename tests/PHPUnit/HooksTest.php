8c53e244933783ca077983532a6e69a188bb0f4f
John P Bloch <johnpbloch@gmail.com>
2015-02-12 23:21:07 -0600

    Add a test for addFilter on a public method

--- tests/PHPUnit/HooksTest.php
+++ tests/PHPUnit/HooksTest.php
@@ -2,8 +2,69 @@
 
 namespace JPB\Tests\WP\Dev;
 
+use JPB\Mocks\WP\Dev\Hookable;
+use WP_Mock;
 use WP_Mock\Tools\TestCase;
 
 class HooksTest extends TestCase {
+
+	public function testAddsHooks() {
+		$this->mockHookFunctions();
+		$expectedHook     = 'the_title';
+		$expectedPriority = rand( 9, 11 );
+		$expectedArgCount = rand( 1, 2 );
+		WP_Mock::wpFunction( 'JPB\\WP\\Dev\\add_filter', [
+			'times'  => 1,
+			'return' => function (
+				$hook,
+				$func,
+				$priority,
+				$argCount
+			) use (
+				$expectedHook,
+				$expectedPriority,
+				$expectedArgCount
+			) {
+				$this->assertEquals( $expectedHook, $hook );
+				$this->assertEquals( $expectedPriority, $priority );
+				$this->assertEquals( $expectedArgCount, $argCount );
+				$title = 'A Title';
+				$this->assertEquals( "<h2>$title</h2>", $func( $title ) );
+			}
+		] );
+
+		$hookable = new Hookable();
+		$hookable->addTitleFilter( $expectedPriority, $expectedArgCount );
+		$this->assertConditionsMet();
+	}
+
+	protected function mockHookFunctions() {
+		WP_Mock::wpFunction( '_wp_filter_build_unique_id', [
+			/*
+			 * Lifted entirely (with modifications) from WordPress core itself
+			 */
+			'return' => function () {
+				$function = func_get_arg( 1 );
+				if ( is_string( $function ) ) {
+					return $function;
+				}
+
+				if ( is_object( $function ) ) {
+					// Closures are currently implemented as objects
+					$function = array( $function, '' );
+				} else {
+					$function = (array) $function;
+				}
+
+				if ( is_object( $function[0] ) ) {
+					// Object Class Calling
+					return spl_object_hash( $function[0] ) . $function[1];
+				} else {
+					// Static Calling
+					return $function[0] . '::' . $function[1];
+				}
+			}
+		] );
+	}
 
 }
--- tests/mocks/Hookable.php
+++ tests/mocks/Hookable.php
@@ -8,4 +8,12 @@ class Hookable {
 
 	use Hooks;
 
+	public function addTitleFilter( $p, $c ) {
+		$this->addFilter( 'the_title', 'title_header', $p, $c );
+	}
+
+	public function title_header( $title ) {
+		return "<h2>$title</h2>";
+	}
+
 }
